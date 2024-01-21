ef00c21bb98d986fe0016a62038172df96e5706c
kernyan <kernyan@users.noreply.github.com>
2024-01-20 20:52:43 -0500

    ban channel and title

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # PureYt
-Chromium based YouTube extension that removes "shorts" and "trending" sections
+Chromium based YouTube extension that
+- removes "shorts" and "trending" sections
+- removes videos whose title contains banned regex expression in banned.json
+- removes videos from channels in banned.json
+
+## Usage
+In browser, enable developer mode in Extension tab and load this directory.
+
+## After
+<img src="./res/after.png" width="600" height="600">
+
+## Before
+<img src="./res/before.png" width="600" height="600">
+
---
+++ content.js
@@ -0,0 +1,64 @@
+// remove element by tag
+function removeElementsByTag(tagName){
+    const elements = document.getElementsByTagName(tagName);
+    while(elements.length > 0){
+        elements[0].parentNode.removeChild(elements[0]);
+    }
+}
+
+let bannedRegex = [];
+let bannedChannels = [];
+// load banned regex expressions
+async function loadRegexPatterns(){
+    try {
+        const response = await fetch(chrome.extension.getURL('banned.json'));
+        const data = await response.json();
+        bannedRegex = data.banWords.map(pattern => new RegExp(pattern));
+        bannedChannels = data.banChannels;
+    } catch (error) {
+        console.error('Error fetching banned tokens: ', error);
+    }
+}
+
+// test if contain banned channel
+function removeBannedChannel(element) {
+    const hrefs = Array.from(element.getElementsByTagName('a')).map(a => a.getAttribute('href'));
+    return hrefs.some(href => bannedChannels.some(word => href.includes('/@${word}')));
+}
+
+// remove elements containing banned title or channel
+function removeBanned() {
+    const vidElems = document.getElementsByTagName("ytd-rich-item-renderer");
+    const elemArray = Array.from(vidElems);
+
+    elemArray.forEach(elem => {
+        const targetElems = elem.querySelectorAll('#video-title');
+        let removeThisElem = false;
+
+        // test if banned regex in video title
+        targetElems.forEach(targetElem => {
+            bannedRegex.forEach(regex => {
+                if (regex.test(targetElem.textContent)) {
+                    removeThisElem = true;
+                }
+            });
+        });
+
+        if (removeBannedChannel(elem)) {
+            removeThisElem = true;
+        }
+
+        if (removeThisElem) {
+            elem.parentNode.removeChild(elem);
+        }
+    });
+}
+
+// Call the function periodically to handle dynamic content
+loadRegexPatterns().then(() => {
+    setInterval(() => {
+        removeElementsByTag('ytd-rich-section-renderer'); // "Shorts", and "Trending" sections' tag
+        removeBanned(); // remove videos with title containing banned regex
+    }, 300); // 0.3 s
+});
+
